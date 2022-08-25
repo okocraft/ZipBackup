477cde28a3c6e7afae154331e1102f6f6f6152e6
Siroshun09 <siro.shun8@gmail.com>
2022-08-26 07:48:47 +0900

    clean: remove maximum files setting

--- src/main/java/net/okocraft/zipbackup/task/purge/BackupPurgeTask.java
+++ src/main/java/net/okocraft/zipbackup/task/purge/BackupPurgeTask.java
@@ -12,7 +12,6 @@ import java.time.Duration;
 import java.time.Instant;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Level;
-import java.util.stream.Collectors;
 
 public class BackupPurgeTask implements Runnable {
 
@@ -77,53 +76,6 @@ public class BackupPurgeTask implements Runnable {
             return deleted.intValue();
         }
 
-        int max = plugin.getConfiguration().get(Settings.BACKUP_PURGE_MAXIMUM_FILES);
-        boolean shouldDelete = false;
-
-        try (var currentFiles = Files.list(directory)) {
-            var count = currentFiles.count();
-
-            if (count == 0) {
-                Files.deleteIfExists(directory);
-                return deleted.intValue();
-            } else {
-                shouldDelete = max < count;
-            }
-        } catch (IOException exception) {
-            plugin.getLogger().log(
-                    Level.SEVERE,
-                    "An error occurred while deleting directory (" + directory.toAbsolutePath() + ")",
-                    exception
-            );
-        }
-
-        if (shouldDelete) {
-            try (var list = Files.list(directory)) {
-                var files =
-                        list.map(BackupFile::new)
-                                .sorted()
-                                .map(BackupFile::getPath)
-                                .collect(Collectors.toList());
-
-                while (!files.isEmpty() && max < files.size()) {
-                    var path = files.get(0);
-
-                    if (Files.isRegularFile(path)) {
-                        Files.delete(path);
-                        deleted.incrementAndGet();
-                    }
-
-                    files.remove(0);
-                }
-            } catch (IOException exception) {
-                plugin.getLogger().log(
-                        Level.SEVERE,
-                        "An error occurred while deleting file.",
-                        exception
-                );
-            }
-        }
-
         return deleted.intValue();
     }
 
@@ -134,31 +86,6 @@ public class BackupPurgeTask implements Runnable {
         } catch (IOException exception) {
             plugin.getLogger().log(Level.SEVERE, "Failed to check file, ignore " + path.toAbsolutePath(), exception);
             return false;
-        }
-    }
-
-    private static class BackupFile implements Comparable<BackupFile> {
-
-        private final Path path;
-        private long lastModification;
-
-        private BackupFile(@NotNull Path path) {
-            this.path = path;
-
-            try {
-                this.lastModification = Files.getLastModifiedTime(path).toMillis();
-            } catch (IOException ignored) {
-                this.lastModification = System.currentTimeMillis();
-            }
-        }
-
-        public @NotNull Path getPath() {
-            return path;
-        }
-
-        @Override
-        public int compareTo(@NotNull BackupFile other) {
-            return Long.compare(lastModification, other.lastModification);
         }
     }
 }
